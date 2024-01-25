40b9c01f0be19c572a53ebd340dbac8cf2e75ad4
Fabowale Awwal <fabowalemuhawwal@gmail.com>
2024-01-25 15:56:59 +0100

    feat: push

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -10,19 +10,16 @@
                     </div>
                     <div class="flex space-x-[10px] items-center">
                         <div class="text-[#3a3f51] leading-[22px] font-[500] text-[14px] plus">
-                            Short by:
+                            Sort by:
                         </div>
                         <div
-                            class="w-fit cursor-pointer flex items-center border-[#e1dfdf] border-[1px] rounded-[20px] py-[6px] px-[12px]">
-                            <div class="text-[#3a3f51] plus text-[12px] leading-[16px] font-[400]">Weekly</div>
-                            <div>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="21" viewBox="0 0 20 21"
-                                    fill="none">
-                                    <path
-                                        d="M3.72456 7.14131C3.94645 6.91942 4.29367 6.89925 4.53835 7.0808L4.60845 7.14131L9.99984 12.5324L15.3912 7.14131C15.6131 6.91942 15.9603 6.89925 16.205 7.0808L16.2751 7.14131C16.497 7.3632 16.5172 7.71042 16.3356 7.9551L16.2751 8.02519L10.4418 13.8585C10.2199 14.0804 9.87267 14.1006 9.62799 13.919L9.5579 13.8585L3.72456 8.02519C3.48048 7.78112 3.48048 7.38539 3.72456 7.14131Z"
-                                        fill="black" />
-                                </svg>
-                            </div>
+                            class="w-fit flex items-center border-[#e1dfdf] border-[1px] rounded-[20px] py-[6px] px-[12px]">
+                            <select class="text-[#3a3f51] plus text-[12px] leading-[16px] font-[400] focus:outline-none"
+                                id="change">
+                                <option>Weekly</option>
+                                <option>Daily</option>
+                                <option>Monthly</option>
+                            </select>
                         </div>
                     </div>
                 </div>
